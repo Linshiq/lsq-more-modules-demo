9f9f3ae394c25e59590eca0413f7137f88443eaa
Linshiq <284244633@qq.com>
2019-06-12 16:13:46 +0800

    首次提交多模块构造信息

---
+++ lsq-modules1/src/main/java/com/lsq/lsqweb/LsqWebApplication.java
@@ -0,0 +1,13 @@
+package com.lsq.lsqweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsqWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LsqWebApplication.class, args);
+	}
+
+}
---
+++ lsq-modules1/src/test/java/com/lsq/lsqweb/LsqWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lsq.lsqweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LsqWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
